1edce1edeafa2a3eff2efa772530f5181916db8b
Leo Dirac <leo@groundlight.ai>
2024-11-27 10:43:40 -0800

    Fixing indentation.

--- src/lazy.py
+++ src/lazy.py
@@ -3,16 +3,16 @@ import importlib.util
 from types import ModuleType
 
 def lazy_load(fullname: str) -> ModuleType:
-  try:
-    return sys.modules[fullname]
-  except KeyError:
-    spec = importlib.util.find_spec(fullname)
-    module = importlib.util.module_from_spec(spec)
-    loader = importlib.util.LazyLoader(spec.loader)
-    # Instead of executing the module now, just set the loader
-    module.__loader__ = loader
-    sys.modules[fullname] = module
-    return module
+    try:
+        return sys.modules[fullname]
+    except KeyError:
+        spec = importlib.util.find_spec(fullname)
+        module = importlib.util.module_from_spec(spec)
+        loader = importlib.util.LazyLoader(spec.loader)
+        # Instead of executing the module now, just set the loader
+        module.__loader__ = loader
+        sys.modules[fullname] = module
+        return module
 
 LAZY_MODULES = [
     "science",
